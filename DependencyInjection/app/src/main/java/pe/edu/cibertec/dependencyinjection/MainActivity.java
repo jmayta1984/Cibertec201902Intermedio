f423d34f33cac4f9bb92fa654c6ae98e88588826
jmayta1984 <jmayta@pucp.edu.pe>
2019-06-30 10:09:31 -0500

    Ejercicio sobre inyección de dependencias

--- DependencyInjection/app/src/main/java/pe/edu/cibertec/dependencyinjection/MainActivity.java
+++ DependencyInjection/app/src/main/java/pe/edu/cibertec/dependencyinjection/MainActivity.java
@@ -10,8 +10,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        /* Base
+
         AppPoolWatcher appPoolWatcher = new AppPoolWatcher();
 
+        appPoolWatcher.notify("Ocurrió el evento");
+        */
+
+        // Inyección de constructor
+        /*
+        EmailSender action = new EmailSender();
+        AppPoolWatcher appPoolWatcher = new AppPoolWatcher(action);
+
+        appPoolWatcher.notify("Ocurrió el evento");
+        */
+
+        // Inyección de método
+        /*
+        SmsSender action = new SmsSender();
+        AppPoolWatcher appPoolWatcher = new AppPoolWatcher();
+        appPoolWatcher.notify(action,"Ocurrió el evento");
+        */
+
+        // Inyección de propiedadd
+        EventLogWriter action = new EventLogWriter();
+        AppPoolWatcher appPoolWatcher = new AppPoolWatcher();
+
+        appPoolWatcher.setAction(action);
         appPoolWatcher.notify("Ocurrió el evento");
 
     }
