dd6b48edf3a3ca3e5c8990aa78a8c34aa6bd1322
jmayta1984 <jmayta@pucp.edu.pe>
2019-06-30 09:31:28 -0500

    Ejercicio sobre inyección de dependencias

---
+++ DependencyInjection/app/src/main/java/pe/edu/cibertec/dependencyinjection/EventLogWriter.java
@@ -0,0 +1,9 @@
+package pe.edu.cibertec.dependencyinjection;
+
+public class EventLogWriter implements INotificationAction {
+    
+    @Override
+    public void write(String message) {
+
+    }
+}
